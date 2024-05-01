105c9f9a5b1368abc22adfd9803c6d74fb8ec4bd
anjiwon319 <anjiwon319@kookmin.ac.kr>
2024-05-01 15:44:30 +0900

    [CD-81] Feat: 캐릭터 이미지 컴포넌트 생성

---
+++ front/lib/constants/image.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+List<Image> characterImages = [
+  const Image(image: AssetImage('assets/images/charactrers/carrot.png')),
+  const Image(image: AssetImage('assets/images/charactrers/catus.png')),
+  const Image(image: AssetImage('assets/images/charactrers/chick.png')),
+  const Image(image: AssetImage('assets/images/charactrers/unicorn.png'))
+];
