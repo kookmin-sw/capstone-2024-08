07bc8c8748d7206620ffa4fdd8707495ff56ad47
anjiwon319 <anjiwon319@kookmin.ac.kr>
2024-05-01 15:46:13 +0900

    [CD-81] Feat: 전체 폰트 적용

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:capstone/constants/fonts.dart' as fonts;
 import 'package:capstone/screen/bottom_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -5,12 +6,14 @@ import 'package:get/get.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 void main() async {
   await dotenv.load(fileName: '.env');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(GetMaterialApp(
+      theme: ThemeData(fontFamily: fonts.font), home: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,22 +23,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Capstone',
-      theme: ThemeData(
-        fontFamily: 'KoddiUDOnGothic'
-      ),
-      builder: (context, child) => ResponsiveBreakpoints.builder(
-        child: child!,
-        breakpoints: [
-          const Breakpoint(start: 0, end: 450, name: MOBILE),
-          const Breakpoint(start: 451, end: 800, name: TABLET),
-        ]
-      ),
+      theme: ThemeData(fontFamily: 'KoddiUDOnGothic'),
+      builder: (context, child) =>
+          ResponsiveBreakpoints.builder(child: child!, breakpoints: [
+        const Breakpoint(start: 0, end: 450, name: MOBILE),
+        const Breakpoint(start: 451, end: 800, name: TABLET),
+      ]),
       initialRoute: '/bottom_navigation',
       getPages: [
-        GetPage(
-          name: '/bottom_navigation', 
-          page: () => const BottomNavBar()
-        )
+        GetPage(name: '/bottom_navigation', page: () => const BottomNavBar())
       ],
       debugShowCheckedModeBanner: false,
     );
