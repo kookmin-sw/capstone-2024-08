63ede9ba0a8bac00ad40ea805056b8ddffd11d96
anjiwon319 <anjiwon319@kookmin.ac.kr>
2024-05-03 12:51:03 +0900

    [CD-81] Feat: 홈 화면 최근 대본 연결 제외한 UI 완성

--- front/lib/screen/home/home.dart
+++ front/lib/screen/home/home.dart
@@ -1,7 +1,9 @@
 import 'package:capstone/constants/color.dart' as colors;
 import 'package:capstone/constants/text.dart' as texts;
 import 'package:capstone/screen/setting/setting.dart';
+import 'package:capstone/widget/home/attendance_streak.dart';
 import 'package:capstone/widget/home/character_section.dart';
+import 'package:capstone/widget/script/script_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -23,12 +25,12 @@ class _HomeState extends State<Home> {
     return AppBar(
       backgroundColor: colors.bgrDarkColor,
       // text overflow 반영 필요 : libary로 화면 비율에 따른 조정 반영 필요
-      title: Text("${texts.homeWelcomeMessage}, $nickname님 !",
+      title: Text("${texts.homeWelcomeMessage} $nickname님 !",
           style: const TextStyle(
               color: colors.themeWhiteColor, fontWeight: FontWeight.bold)),
       actions: [
         IconButton(
-          icon: const Icon(Icons.settings, color: Colors.white),
+          icon: const Icon(Icons.settings_rounded, color: Colors.white),
           onPressed: () {
             HapticFeedback.lightImpact();
             Get.to(() => const Setting());
@@ -39,9 +41,22 @@ class _HomeState extends State<Home> {
   }
 
   Widget _characterSection() {
-    return SizedBox(
+    return Container(
         width: MediaQuery.of(context).size.width / 1.7,
         child: characterSection('carrot'));
+  }
+
+  Widget _attendanceStreak() {
+    return Container(child: attendanceStreak(nickname, 8));
+  }
+
+  Widget _lastPracticeScript() {
+    return Container(
+        child: Text(
+      '최근 연습 대본',
+      style: TextStyle(color: colors.themeWhiteColor),
+    ));
+    // return scriptListTile(context, );
   }
 
   @override
@@ -49,8 +64,10 @@ class _HomeState extends State<Home> {
     return Scaffold(
         backgroundColor: colors.bgrDarkColor,
         appBar: homeAppBar(),
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [_characterSection()]));
+        body: Column(children: [
+          _characterSection(),
+          _lastPracticeScript(),
+          _attendanceStreak()
+        ]));
   }
 }
