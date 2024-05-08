1ee7528fca4719662b8a041a707c70bcd5736583
anjiwon319 <anjiwon319@kookmin.ac.kr>
2024-05-09 01:07:51 +0900

    [CD-144] Feat: 음성 파일 저장 위치 temDirectory로 변경

--- front/lib/widget/audio_recoder/platform/audio_recorder_io.dart
+++ front/lib/widget/audio_recoder/platform/audio_recorder_io.dart
@@ -5,7 +5,10 @@ import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
 
 mixin AudioRecorderMixin {
-  Future<void> recordFile(AudioRecorder recorder, RecordConfig config) async {
+  Future<void> recordFile(
+    AudioRecorder recorder,
+    RecordConfig config,
+  ) async {
     final path = await _getPath();
 
     await recorder.start(config, path: path);
@@ -37,7 +40,7 @@ mixin AudioRecorderMixin {
   void downloadWebData(String path) {}
 
   Future<String> _getPath() async {
-    final dir = await getApplicationDocumentsDirectory();
+    final dir = await getTemporaryDirectory();
     return p.join(
       dir.path,
       'audio_${DateTime.now().millisecondsSinceEpoch}.wav',
