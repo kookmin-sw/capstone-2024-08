0f62872e7456e1e88f532c8ef71f4345870bd3ec
Shin-MG <alsruddl0407@kookmin.ac.kr>
2024-05-10 15:32:01 +0900

    [CD-198][CD-199] Feat: create function logout & deleteUser

--- front/lib/screen/setting/setting.dart
+++ front/lib/screen/setting/setting.dart
@@ -1,4 +1,8 @@
+import 'package:capstone/screen/authentication/controller/auth_controller.dart';
 import 'package:capstone/widget/basic_app_bar.dart';
+import 'package:capstone/widget/warning_dialog.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:capstone/constants/color.dart' as colors;
 import 'package:capstone/constants/route.dart' as routes;
@@ -17,11 +21,17 @@ class Setting extends StatelessWidget {
           children: routes.settingItems.map((item) =>
             GestureDetector(
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => item['route'])
-                );
+                if (item.containsKey('action')) {
+                  item['action'](context);
+                }
+                if (item.containsKey('page')) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => item['page'],
+                    ),
+                  );
+                }
               },
               child: Container(
                 width: MediaQuery.of(context).size.width,
@@ -55,3 +65,42 @@ class Setting extends StatelessWidget {
     ));
   }
 }
+
+Future<bool> showConfirmationDialog(BuildContext context, String type) async {
+    return await showDialog(
+      context: context,
+      builder: (BuildContext context) =>
+        WarningDialog(
+          warningObject: type,
+        )
+    );
+  }
+
+
+  Future<void> handleLogoutAction(BuildContext context) async {
+    //AuthController authController = AuthController.instance;
+    bool confirmLogout = await showConfirmationDialog(context, 'logout');
+    print('*******************confirmLogout');
+    print(confirmLogout);
+
+    // if (confirmLogout) {
+    //   authController.logout();
+    // }
+  }
+
+  void handleDeleteAction(BuildContext context) async {
+    //AuthController authController = AuthController.instance;
+    bool confirmDelete = await showConfirmationDialog(context, 'deleteUser');
+    print('*******************confirmDelete');
+    print(confirmDelete);
+
+    // if (confirmDelete) {
+    //   CollectionReference userInfo = FirebaseFirestore.instance.collection('user');
+    //   User? user = FirebaseAuth.instance.currentUser;
+      
+    //   if (user != null) {
+    //     userInfo.doc(user.uid).delete();
+    //     authController.deleteUser(); 
+    //   }
+    // }
+  }
