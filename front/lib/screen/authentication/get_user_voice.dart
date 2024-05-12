d435eae82c21af4e800e8da17cfe3003e96dde4e
Shin-MG <alsruddl0407@kookmin.ac.kr>
2024-05-12 19:15:00 +0900

    [CD-194] Chore: remove back button on get_user_voice

--- front/lib/screen/authentication/get_user_voice.dart
+++ front/lib/screen/authentication/get_user_voice.dart
@@ -188,6 +188,7 @@ class _GetUserVoiceState extends State<GetUserVoice> {
   AppBar getUserVoiceAppBar() {
     return AppBar(
       backgroundColor: colors.bgrDarkColor,
+      automaticallyImplyLeading: false,
       centerTitle: true,
       title: Text(texts.getUserVoiceAppBarTitle,
           style: const TextStyle(
