dc4f1d7d72762568077f5e955939e9787feec213
anjiwon319 <anjiwon319@kookmin.ac.kr>
2024-05-12 09:51:04 +0900

    [CD-211] Feat: 백엔드로 wav 파일들과 텍스트 보내는 코드 추가

---
+++ front/lib/screen/practice/guide_voice_player.dart
@@ -0,0 +1,58 @@
+import 'package:capstone/screen/authentication/controller/user_controller.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:io';
+import 'package:capstone/constants/text.dart' as texts;
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class GuideVoicePlayer extends StatefulWidget {
+  GuideVoicePlayer({Key? key, required this.text}) : super(key: key);
+  String text;
+
+  @override
+  State<GuideVoicePlayer> createState() => _GuideVoicePlayerState();
+}
+
+class _GuideVoicePlayerState extends State<GuideVoicePlayer> {
+  Map<String, File?> _wavFiles = Get.find<UserController>().wavFiles;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> sendDataToServer(
+      String text, Map<String, File?> wavFiles) async {
+    var url = Uri.parse('${texts.baseUrl}/feedback/');
+
+    // 멀티파트 리퀘스트 생성
+    var request = http.MultipartRequest('POST', url);
+
+    // 텍스트 필드 추가
+    request.fields['text'] = widget.text;
+
+    // WAV 파일 추가
+    var wavStream = http.ByteStream(wavFile.openRead());
+    var length = await wavFile.length();
+    var multipartFiles =
+        http.MultipartFile('wavs', wavStream, length, filename: 'audio.wav');
+
+    // WAV 파일을 리퀘스트에 추가
+    request.files.add(multipartFile);
+
+    // 리퀘스트 보내기
+    var response = await request.send();
+
+    // 응답 확인
+    if (response.statusCode == 200) {
+      print('데이터가 성공적으로 서버로 전송되었습니다.');
+    } else {
+      print('데이터 전송에 실패했습니다. 상태 코드: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
