78d2b8de0823c51f148c4a64784586ebe18ad0b1
anjiwon319 <anjiwon319@kookmin.ac.kr>
2024-05-13 21:16:27 +0900

    [CD-207] Feat: 마지막으로 연습한 대본 위젯 연결

--- front/lib/screen/home/home.dart
+++ front/lib/screen/home/home.dart
@@ -1,10 +1,12 @@
 import 'package:capstone/constants/color.dart' as colors;
 import 'package:capstone/constants/text.dart' as texts;
 import 'package:capstone/model/load_data.dart';
+import 'package:capstone/model/script.dart';
 import 'package:capstone/screen/setting/setting.dart';
 import 'package:capstone/screen/authentication/controller/user_controller.dart';
 import 'package:capstone/widget/home/attendance_streak.dart';
 import 'package:capstone/widget/home/character_section.dart';
+import 'package:capstone/widget/script/script_list_tile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -25,7 +27,7 @@ class _HomeState extends State<Home> {
   String? character = Get.find<UserController>().userModel.character;
   int? attendanceStreakDays =
       Get.find<UserController>().userModel.attendanceStreak;
-  DocumentReference? lastPracticeScript =
+  DocumentReference? lastPracticeScriptRef =
       Get.find<UserController>().userModel.lastPracticeScript;
 
   AppBar homeAppBar() {
@@ -64,34 +66,35 @@ class _HomeState extends State<Home> {
   }
 
   Widget _lastPracticeScript(
-      BuildContext context, DocumentReference? lastPracticeScript) {
-    if (lastPracticeScript == null) {
+      BuildContext context, DocumentReference? documentRef) {
+    if (documentRef == null) {
       return Text(
         texts.lastPracticeScriptEmptyMessage,
         style: const TextStyle(color: colors.themeWhiteColor),
       );
     } else {
-      return Container();
-      // return FutureBuilder<ScriptModel?>(
-      //   future: loadData.readScriptByDocumentRef(lastPracticeScript),
-      //   builder: (context, snapshot) {
-      //     if (snapshot.connectionState == ConnectionState.waiting) {
-      //       return const CircularProgressIndicator(
-      //         color: colors.exampleScriptColor,
-      //       ); // 로딩 중이면 로딩 인디케이터 표시
-      //     } else if (snapshot.hasError) {
-      //       return Text('Error: ${snapshot.error}'); // 에러가 있으면 에러 메시지 표시
-      //     } else {
-      //       // 데이터가 로드되면 스크립트를 표시
-      //       final script = snapshot.data;
-      //       if (script != null) {
-      //         return scriptListTile(context, script);
-      //       } else {
-      //         return Text('No script found'); // 스크립트가 없으면 해당 메시지 표시
-      //       }
-      //     }
-      //   },
-      // );
+      String scriptType = documentRef.path.split('_')[0];
+      return FutureBuilder<ScriptModel?>(
+        future: loadData.readScriptByDocumentRef(documentRef),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator(
+              color: colors.exampleScriptColor,
+            ); // 로딩 중이면 로딩 인디케이터 표시
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}'); // 에러가 있으면 에러 메시지 표시
+          } else {
+            // 데이터가 로드되면 스크립트를 표시
+            final script = snapshot.data;
+            if (script != null) {
+              return scriptListTile(context, script, 'script', scriptType);
+              ;
+            } else {
+              return Text('No script found'); // 스크립트가 없으면 해당 메시지 표시
+            }
+          }
+        },
+      );
     }
   }
 
@@ -102,7 +105,7 @@ class _HomeState extends State<Home> {
         appBar: homeAppBar(),
         body: Column(children: [
           _characterSection(character),
-          _lastPracticeScript(context, lastPracticeScript),
+          _lastPracticeScript(context, lastPracticeScriptRef),
           _attendanceStreak(nickname, attendanceStreakDays)
         ]));
   }
