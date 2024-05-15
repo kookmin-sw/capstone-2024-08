88e7fe35a0377f147db6d9f53afc5cdc0eeab614
Pilmo <pilmokim99@gmail.com>
2024-05-15 17:14:10 +0900

    [CD-167] Feat: change file path

--- backend/voice_conversion/__init__.py
+++ backend/voice_conversion/__init__.py
@@ -3,14 +3,13 @@ from typing import List
 
 
 def change_voice(src: str, ref: List[str]):
+    """
+    src: path to 16kHz, single-channel, source waveform
+    ref: list of paths to all reference waveforms (each must be 16kHz, single-channel) from the target speaker
+    """
     knn_vc = torch.hub.load('bshall/knn-vc', 'knn_vc', prematched=True, trust_repo=True, pretrained=True, device='cuda')
-    # path to 16kHz, single-channel, source waveform
-    src_wav_path = "/home/ubuntu/forked/capstone-2024-08/backend/voice_converison/SPK064KBSCU001M001.wav"
-    # list of paths to all reference waveforms (each must be 16kHz, single-channel) from the target speaker
-    ref_wav_paths = ["/home/ubuntu/forked/capstone-2024-08/backend/voice_converison/SPK014KBSCU004F002.wav"]
-
-    query_seq = knn_vc.get_features(src_wav_path)
-    matching_set = knn_vc.get_matching_set(ref_wav_paths)
+    query_seq = knn_vc.get_features(src)
+    matching_set = knn_vc.get_matching_set(ref)
     out_wav = knn_vc.match(query_seq, matching_set, topk=4)
     torchaudio.save("/home/ubuntu/forked/capstone-2024-08/backend/voice_converison/vc_out.wav", out_wav[None], 16000)
-    return "/home/ubuntu/forked/capstone-2024-08/backend/voice_converison/vc_out.wav"
+    return "http://ec2-13-124-219-249.ap-northeast-2.compute.amazonaws.com/static/vc_out.wav"
